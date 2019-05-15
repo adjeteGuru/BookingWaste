f00283107aaea36ec573a3ae5bdc10b0a650f4a9
adjeteGuru <justinadjev@hotmail.com>
2019-05-15 19:50:12 +0100

    Master code

---
+++ BookingWaste/BookingWaste/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookingWaste.Startup))]
+namespace BookingWaste
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
