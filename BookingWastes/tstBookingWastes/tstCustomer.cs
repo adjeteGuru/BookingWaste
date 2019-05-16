fb0f2a5197bbfb47049057596d2106fb9f890d30
adjeteGuru <justinadjev@hotmail.com>
2019-05-16 21:16:03 +0100

    testplan added

---
+++ BookingWastes/tstBookingWastes/tstCustomer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BWClassLibrary;
+
+namespace tstBookingWastes
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {//create an instance of the class
+            clsCustomer ACustomer = new clsCustomer();
+            //test result
+            Assert.IsNotNull(ACustomer);
+        }
+    }
+}
---
+++ BookingWastes/tstBookingWastes/tstSecurity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BWClassLibrary;
+
+namespace tstBookingWastes
+{
+    [TestClass]
+    public class tstSecurity
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {//create an instance of the class
+            clsSecurity ASec = new clsSecurity();
+            //test the result
+            Assert.IsNotNull(ASec);
+        }
+    }
+}
