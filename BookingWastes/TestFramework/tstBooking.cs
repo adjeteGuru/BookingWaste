5ad62fe6934c3916a717cd3a317a4f8828045ec6
adjeteGuru <justinadjev@hotmail.com>
2019-05-16 22:18:56 +0100

    firstTest

---
+++ BookingWastes/TestFramework/tstBooking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestFramework
+{
+    [TestClass]
+    public class tstBooking
+    {
+        [TestMethod]
+        public void InstanceOk()
+        
+        {
+
+
+        }
+    }
+}
