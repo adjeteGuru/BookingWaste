7f4c448574a08c3966b1836661ad5759d7140135
adjeteGuru <justinadjev@hotmail.com>
2019-05-16 22:15:27 +0100

    TestFramework added

--- BookingWastes/BWClassLibrary/clsCustomerCollection.cs
+++ BookingWastes/BWClassLibrary/clsCustomerCollection.cs
@@ -1,4 +1,10 @@
-﻿namespace BWClassLibrary
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BWClassLibrary
 {
     public class clsCustomerCollection
     {
